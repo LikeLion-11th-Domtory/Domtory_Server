3c6d885e5fe76a2490f45279e333662430eac9ad
Leespect <wewqwew153@gmail.com>
2023-11-24 13:33:24 +0900

    Feat: menu app 등록, menu의 각 model에 max_length 설정

--- menu/models.py
+++ menu/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Menu(models.Model):
     id = models.AutoField(primary_key=True)
-    date_code = models.CharField()  # yymmdd: 231125
-    date_detail = models.CharField()  # 23.11.19 (일)
-    breakfast = models.CharField()
-    lunch = models.CharField()
-    dinner = models.CharField()
+    date_code = models.CharField(max_length=6)  # yymmdd: 231125
+    date_detail = models.CharField(max_length=15)  # 23.11.19 (일)
+    breakfast = models.CharField(max_length=1000)
+    lunch = models.CharField(max_length=1000)
+    dinner = models.CharField(max_length=1000)
     
     def __str__(self):
         return self.date
