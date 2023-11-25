8ff65dd1a12afd65ce58cf9446b2b95234b9c82e
Leespect <wewqwew153@gmail.com>
2023-11-25 16:54:42 +0900

    Refactor: 식단 view를 하나로 통합

--- menu/urls.py
+++ menu/urls.py
@@ -4,10 +4,5 @@ from .views import *
 app_name = 'menu'
 
 urlpatterns = [
-    
-    path('list/<str:date_code>/', MenuListView.as_view(), name='menu_list'),
-    path('breakfast/<str:date_code>/', BreakfastView.as_view(), name='breakfast'),
-    path('lunch/<str:date_code>/', LunchView.as_view(), name='lunch'),
-    path('dinner/<str:date_code>/', DinnerView.as_view(), name='dinner'),
-    
+    path('<str:date_code>/<str:option>/', MenuListView.as_view(), name='menu_list'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -6,33 +6,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-# 날짜에 맞는 전체 식단 조회
+# 날짜에 맞는 식단 조회: total이면 전부 조회, 아니면 아침, 점심, 저녁 중 하나만 조회
 class MenuListView(APIView):
-    def get(self, request, date_code):
+    def get(self, request, date_code, option):
         menus = Menu.objects.filter(date_code=date_code)
-        serializer = MenuSerializer(menus, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-
-# 날짜에 맞는 아침 식단 조회
-class BreakfastView(APIView):
-    def get(self, request, date_code):
-        menus = Menu.objects.filter(date_code=date_code)
-        serializer = BreakfastSerializer(menus, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# 날짜에 맞는 점심 식단 조회
-class LunchView(APIView):
-    def get(self, request, date_code):
-        menus = Menu.objects.filter(date_code=date_code)
-        serializer = LunchSerializer(menus, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# 날짜에 맞는 저녁 식단 조회
-class DinnerView(APIView):
-    def get(self, request, date_code):
-        menus = Menu.objects.filter(date_code=date_code)
-        serializer = DinnerSerializer(menus, many=True)
+        if option == 'total':
+            serializer = MenuSerializer(menus, many=True)
+        elif option == 'breakfast':
+            serializer = BreakfastSerializer(menus, many=True)
+        elif option == 'lunch':
+            serializer = LunchSerializer(menus, many=True)
+        elif option == 'dinner':
+            serializer = DinnerSerializer(menus, many=True)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.data, status=status.HTTP_200_OK)
