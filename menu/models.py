160117aa7f88a477fd201794740942641587ed3c
Leespect <wewqwew153@gmail.com>
2023-11-26 20:31:07 +0900

    Refactor: 아침, 점심, 저녁 메뉴의 max_length를 512로 수정

--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Menu(models.Model):
     date_code = models.CharField(primary_key=True, max_length=6)  # yymmdd: 231125
     date_detail = models.CharField(max_length=15)  # 23.11.19 (일)
-    breakfast = models.CharField(max_length=1000)
-    lunch = models.CharField(max_length=1000)
-    dinner = models.CharField(max_length=1000)
+    breakfast = models.CharField(max_length=512)
+    lunch = models.CharField(max_length=512)
+    dinner = models.CharField(max_length=512)
     
     def __str__(self):
         return self.date
