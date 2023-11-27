8275b3f2f5fef61e471b76d5e90121e61a5454f2
Leespect <wewqwew153@gmail.com>
2023-11-27 18:13:59 +0900

    Refactor: 크롤링 코드를 class view로 수정하여 api화

--- menu/urls.py
+++ menu/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import *
-from .crawling_view import crawling
+from .crawling_view import CrawlingView
 
 app_name = 'menu'
 
 urlpatterns = [
     path('<str:date_code>/<str:option>/', MenuListView.as_view(), name='menu_list'),
-    path('crawling/', crawling, name='crawling')
+    path('crawling/', CrawlingView.as_view(), name='crawling')
 ]
