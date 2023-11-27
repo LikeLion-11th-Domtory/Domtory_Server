838fb61d2f10fe033e43b068b458bfb74f4cd54d
Leespect <wewqwew153@gmail.com>
2023-11-27 18:21:40 +0900

    Refactor: menu 모델을 조식/중식/석식에 해당하는 각 모델로 세붕화

--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,33 @@ from django.db import models
 class Menu(models.Model):
     date_code = models.CharField(primary_key=True, max_length=6)  # yymmdd: 231125
     date_detail = models.CharField(max_length=15)  # 23.11.19 (일)
-    breakfast = models.CharField(max_length=512)
-    lunch = models.CharField(max_length=512)
-    dinner = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.date_code
+
+
+class Breakfast(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_code = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    menu = models.CharField(max_length=512)
     
     def __str__(self):
-        return self.date
+        return self.menu
+
+
+class Lunch(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_code = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    menu = models.CharField(max_length=512)
+    
+    def __str__(self):
+        return self.menu
+
+
+class Dinner(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_code = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    menu = models.CharField(max_length=512)
+    
+    def __str__(self):
+        return self.menu
