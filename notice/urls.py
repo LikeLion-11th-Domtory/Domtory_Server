f16a78f952df74b4621fc632a4d3dee84535d0c2
Lee Jae Yeon <bestfourteen14@gmail.com>
2023-11-28 12:17:54 +0900

    Fix: notice url.py 수정

--- notice/urls.py
+++ notice/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'notice'
 
 urlpatterns = [
-    path('notice/', NoticeListView.as_view(), name='notice_list'),
-    path('notice/<int:id>/', NoticeDetailView.as_view(), name='notice_detail'),
+    path('', NoticeListView.as_view(), name='notice_list'),
+    path('<int:id>/', NoticeDetailView.as_view(), name='notice_detail'),
 ]
