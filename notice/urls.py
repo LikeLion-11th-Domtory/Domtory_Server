00eb4f092b49927ab31e5c84c055f3bb468f386d
Lee Jae Yeon <bestfourteen14@gmail.com>
2023-11-30 02:06:22 +0900

    feat: 공지사항 수동 크롤링 추가

--- notice/urls.py
+++ notice/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
+from .parser_view import *
 
 app_name = 'notice'
 
 urlpatterns = [
+    path('parser/', CrawlDataView.as_view(), name='parser_view'),
     path('', NoticeListView.as_view(), name='notice_list'),
     path('<int:id>/', NoticeDetailView.as_view(), name='notice_detail'),
 ]
