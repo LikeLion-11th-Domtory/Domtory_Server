c712dee54dd156cb70e31d88ab84ffcaf7db3f18
leehjhjhj <tsukiakarii@naver.com>
2024-01-28 23:07:32 +0900

    refactor: 공지사항 크롤러 반환 삭제

--- notice/parser_view.py
+++ notice/parser_view.py
@@ -31,7 +31,7 @@ class CrawlDataView(APIView):
                     content=post['content'],
                     images=post['images']).save()
 
-        return Response(all_posts_data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
 
     def scrape_page(self, page):
         # Construct the URL for the specific page
