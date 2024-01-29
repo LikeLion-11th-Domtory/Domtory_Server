16dbc9ea262f30a5ded453ff567aeec457332be8
leehjhjhj <tsukiakarii@naver.com>
2024-01-29 19:02:08 +0900

    feat: swagger 추가

--- push/views/push_views.py
+++ push/views/push_views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from push.containers import PushContainer
+from drf_yasg.utils import swagger_auto_schema
+from push.serializers import TokenSendRequestSerializer
 
 class PushView(APIView):
 
@@ -9,6 +11,10 @@ class PushView(APIView):
         super().__init__(*args, **kwargs)
         self._push_service = PushContainer.push_service()
 
+    @swagger_auto_schema(request_body=TokenSendRequestSerializer)
     def post(self, request):
+        """
+        FCM 푸시 토큰을 서버로 보내는 API 입니다.
+        """
         self._push_service.send_push_token(request.data)
         return Response(status=status.HTTP_200_OK)
--- push/views/token_invalid_view.py
+++ push/views/token_invalid_view.py
@@ -10,5 +10,8 @@ class TokenInvalidView(APIView):
         self._push_service = PushContainer.push_service()
 
     def post(self, request):
+        """
+        삭제 예정 API
+        """
         self._push_service.make_token_invalid(request.data)
         return Response(status=status.HTTP_200_OK)
