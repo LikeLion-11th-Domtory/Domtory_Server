24cce921c3b8a98f9911709039958c8a55e875fe
leehjhjhj <tsukiakarii@naver.com>
2024-02-08 13:17:29 +0900

    refactor: serializer 이름변경

--- push/serializers/push_serializers.py
+++ push/serializers/push_serializers.py
@@ -1,4 +1,4 @@
 from rest_framework import serializers
 
-class TokenSendRequestSerializer(serializers.Serializer):
+class TokenRequestSerializer(serializers.Serializer):
     pushToken = serializers.CharField(source='push_token')
