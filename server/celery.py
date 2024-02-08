9ae9dbb0e11496c6c46c6fd98b4f6201a2dc8e3a
leehjhjhj <tsukiakarii@naver.com>
2024-02-08 13:16:15 +0900

    feat: push task 추가

--- server/celery.py
+++ server/celery.py
@@ -8,10 +8,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTINGS_MODULE')
 app = Celery('domtory')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks() ## task.py 이외에서는 ['custom.py'] 이렇게 추가해줘야 함
+app.autodiscover_tasks(['push.tasks',])
 
 app.conf.broker_transport_options = {'visibility_timeout': 31536000}
 
 cred_path = config("FIRE_BASE_JSON_KEY_PATH")
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred, name='celery_firebase_app')
+
+app.conf.broker_connection_retry_on_startup = True
+
+
+#celery -A server worker -P gevent -c 100 --loglevel=info
