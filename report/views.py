ffa73b57e0cfcb1ac67cd1e2232e253987258d7e
Chaeyoung714 <202102760@hufs.ac.kr>
2024-02-12 01:36:41 +0900

    [feat] report 기능 추가

--- report/views.py
+++ report/views.py
@@ -7,6 +7,8 @@ import requests
 from board.models import *
 from .serializers import *
 
+import json
+
 # Create your views here.
 
 class CreateReportView(APIView):
@@ -31,10 +33,13 @@ class CreateReportView(APIView):
                 'target_id' : target.id,
                 'table' : target_type,
                 'report_id' : serializer.data['id']
-                }
-            response = requests.post('https://8ufbqa4zl8.execute-api.ap-northeast-2.amazonaws.com/prod', data=dataset)
+            }
+            # dataset = json.dumps(dataset, ensure_ascii=False)
+            response = requests.post('https://8ufbqa4zl8.execute-api.ap-northeast-2.amazonaws.com/prod', json=dataset)
+            print(response.content)
+            print(dataset)
 
-            return Response(response, serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # #개발 편의용
