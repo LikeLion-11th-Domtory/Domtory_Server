a9755f2ec0041ac14fb85609ab09f030c73d417e
leehjhjhj <tsukiakarii@naver.com>
2024-02-11 13:03:48 +0900

    chore: redoc authorization_header 추가

--- member/presentations/withdrawal_view.py
+++ member/presentations/withdrawal_view.py
@@ -1,12 +1,24 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from member.containers import MembersContainer
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from drf_yasg.utils import no_body
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
-class WithdrawalView(TokenObtainPairView):
+authorization_header = openapi.Parameter(
+    'Authorization', 
+    openapi.IN_HEADER, 
+    description="Bearer <token>", 
+    type=openapi.TYPE_STRING,
+    required=True
+)
+
+
+class WithdrawalView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -14,8 +26,8 @@ class WithdrawalView(TokenObtainPairView):
         super().__init__(*args, **kwargs)
         self._members_service = MembersContainer.member_service()
 
-    @swagger_auto_schema(responses={"204": ""})
-    def post(self, request, *args, **kwargs):
+    @swagger_auto_schema(manual_parameters=[authorization_header], request_body=no_body, responses={"204": ""})
+    def post(self, request):
         """"
         회원 탈퇴 API 입니다.
         """
