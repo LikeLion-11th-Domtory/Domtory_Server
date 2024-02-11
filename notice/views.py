ae81bf525b6cd8d732e158cf8308dcf9275ee5f7
Chaeyoung714 <202102760@hufs.ac.kr>
2024-02-12 02:11:28 +0900

    [feat] get /notice/시 pagination 추가

--- notice/views.py
+++ notice/views.py
@@ -3,11 +3,30 @@ from .models import NoticeList
 from .serializers import NoticeListSerializer
 from rest_framework import generics
 
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from collections import OrderedDict
+
+
 # Create your views here.
 
+#페이지네이션
+class PostPageNumberPagination(PageNumberPagination):
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('pageCnt', self.page.paginator.num_pages),
+            ('curPage', self.page.number),
+            ('postList', data),
+        ]))
+    
 class NoticeListView(generics.ListAPIView):
     queryset = NoticeList.objects.all().order_by('-date')
     serializer_class = NoticeListSerializer
+
+    #페이지네이션
+    pagination_class = PostPageNumberPagination
 
 class NoticeDetailView(generics.RetrieveAPIView):
     queryset = NoticeList.objects.all()
