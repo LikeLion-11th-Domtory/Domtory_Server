ab133192ade8dd542047204cfd7915c7d4c36605
leehjhjhj <tsukiakarii@naver.com>
2024-02-11 21:52:14 +0900

    feat: 토큰 삭제 관련 레포 로직 추가

--- push/domains/push_repository.py
+++ push/domains/push_repository.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
 from push.domains.device import Device
+from member.domains import Member
 
 class PushRepository:
     
     def save_device(self, device: Device):
         device.save()
 
-    def find_device_by_token(self, device_token: str):
-        return get_object_or_404(Device, device_token=device_token)
+    def find_device_by_token_and_member(self, device_token: str, member: Member):
+        return get_object_or_404(Device, device_token=device_token, member=member)
     
     def find_all_valid_device(self):
         return Device.objects.filter(is_valid=True)
@@ -16,4 +17,4 @@ class PushRepository:
         pass
 
     def delete_device(self, device: Device):
-        Device.delete()
+        device.delete()
