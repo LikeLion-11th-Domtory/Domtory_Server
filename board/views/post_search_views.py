cbec9693778c847a77175f7138ea4292e5393e69
ShinSub98 <wjdtjq26@naver.com>
2024-02-12 03:44:47 +0900

    feat: 검색 시 게시판 선택 기능 추가

--- board/views/post_search_views.py
+++ board/views/post_search_views.py
@@ -14,11 +14,15 @@ class PostSearchView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     
-    def post(self, request):
+    def post(self, request, board_id):
         word_list = request.data['word_list']
         query = Q()
-        for keyword in word_list:
+
+        for keyword in word_list: # Q 객체에 검색어 추가
             query |= Q(body__icontains=keyword) | Q(title__icontains=keyword)
-        
-        posts = Post.objects.filter(query)
+
+        if board_id != 0: # 특정 게시판에 대하여 검색 시
+            posts = Post.objects.filter(Q(board = board_id) & query)
+        else: # 전체 게시판에 대하여 검색 시
+            posts = Post.objects.filter(query)
         return Response(PostResponseSerializer(posts, many = True).data)
