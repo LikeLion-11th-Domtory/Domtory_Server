11e68bfcac6c8f875de09022ea3d1df35dad7109
Chaeyoung714 <202102760@hufs.ac.kr>
2024-02-12 23:24:33 +0900

    [feat] django admin에서 Report.status = valid/invalid 설정 시 Post.is_blocked = True/False로 변경됨

--- report/admin.py
+++ report/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Report
+from board.models.post_models import Post
+from board.models.comment_models import Comment
 
 # Register your models here.
 
-admin.site.register(Report)
+
+class ReportAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if change:
+            if obj.post:
+                if obj.status == Report.ReportType.VALID:
+                    obj.post.is_blocked = True
+                elif obj.status == Report.ReportType.INVALID:
+                    obj.post.is_blocked = False
+                obj.post.save()
+            if obj.comment:
+                if obj.status == Report.ReportType.VALID:
+                    obj.comment.is_blocked = True
+                elif obj.status == Report.ReportType.INVALID:
+                    obj.comment.is_blocked = False
+                obj.comment.save()
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Report, ReportAdmin)
