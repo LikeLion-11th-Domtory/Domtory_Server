0c05e1648516d56593a6e1ed855efd3e10c6cfeb
leehjhjhj <tsukiakarii@naver.com>
2024-02-16 08:30:16 +0900

    chore: storage 관련 설정 추가

--- server/settings/base.py
+++ server/settings/base.py
@@ -155,6 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('BUCKET_NAME')
 
 #######celery#######
 CELERY_TIMEZONE = 'Asia/Seoul'
