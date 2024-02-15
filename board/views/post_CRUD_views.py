1eba72ccbf22425eb2cee0f4d7afc22a7d7d6453
ShinSub98 <wjdtjq26@naver.com>
2024-02-15 16:08:17 +0900

    refactor: S3 이미지 업로드 key 양식 수정

--- board/views/post_CRUD_views.py
+++ board/views/post_CRUD_views.py
@@ -57,7 +57,7 @@ class PostCreateView(APIView):
             image.save(buffer, format = 'JPEG', quality = 80)
             image_data = buffer.getvalue()
 
-            key = f"{post.board.name}_{post.pk}_{uuid.uuid4().hex}.jpeg"
+            key = f"{post.board.name}/{post.pk}_{uuid.uuid4().hex}.jpeg"
             image_url = s3.upload_to_s3(image_data = image_data, key = key, content_type = 'image/jpeg')
             
             PostImage(post = post, image_url = image_url).save()
@@ -131,7 +131,7 @@ class PostUpdateView(APIView):
             image.save(buffer, format = 'JPEG', quality = 80)
             image_data = buffer.getvalue()
 
-            key = f"{post.board.name}_{post.pk}_{uuid.uuid4().hex}.jpeg"
+            key = f"{post.board.name}/{post.pk}_{uuid.uuid4().hex}.jpeg"
             image_url = s3.upload_to_s3(image_data = image_data, key = key, content_type = 'image/jpeg')
             
             PostImage(post = post, image_url = image_url).save()
