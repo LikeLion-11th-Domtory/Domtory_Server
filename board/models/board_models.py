51cd6fbb7b664affd6921b9a668188aef1a3e586
ShinSub98 <wjdtjq26@naver.com>
2024-02-17 02:47:51 +0900

    refactor: verbose_name 추가

--- board/models/board_models.py
+++ board/models/board_models.py
@@ -4,8 +4,8 @@ class Board(models.Model):
     """
     게시판 모델
     """
-    name = models.CharField(max_length = 60, null = False, unique = True) # 한글 20글자
-    description = models.TextField(null = True, blank = True)
+    name = models.CharField(max_length = 60, null = False, unique = True, verbose_name = '게시판 이름') # 한글 20글자
+    description = models.TextField(null = True, blank = True, verbose_name = '설명')
 
     def save(self, *args, **kwargs):
         if self.description == None or self.description == "":
--- board/models/comment_models.py
+++ board/models/comment_models.py
@@ -6,11 +6,11 @@ class Comment(models.Model):
     """
     댓글 모델
     """
-    member = models.ForeignKey(Member, null = False, on_delete = models.CASCADE, related_name = 'comment')
-    post = models.ForeignKey(Post, null = False, blank = True, on_delete = models.CASCADE, related_name = 'comment')
-    parent = models.ForeignKey('self', null = True, blank = True, on_delete = models.PROTECT, related_name = 'reply')
-    body = models.TextField(default = "", null = False)
-    created_at = models.DateTimeField(auto_now_add = True)
+    member = models.ForeignKey(Member, null = False, on_delete = models.CASCADE, verbose_name = '작성자', related_name = 'comment')
+    post = models.ForeignKey(Post, null = False, blank = True, on_delete = models.CASCADE, verbose_name = '게시글', related_name = 'comment')
+    parent = models.ForeignKey('self', null = True, blank = True, on_delete = models.PROTECT, verbose_name = '댓글', related_name = 'reply')
+    body = models.TextField(default = "", verbose_name = '내용', null = False)
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name = '작성일시')
     is_blocked = models.BooleanField(default = False)
     is_deleted = models.BooleanField(default = False)
 
