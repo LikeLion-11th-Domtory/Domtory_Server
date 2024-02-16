820fe7ea34e6ba15f1dad3870c94bed724246db2
Chaeyoung714 <202102760@hufs.ac.kr>
2024-02-16 23:54:13 +0900

    [feat] report/admin.py 고도화

--- report/admin.py
+++ report/admin.py
@@ -3,10 +3,38 @@ from .models import Report
 from board.models.post_models import Post
 from board.models.comment_models import Comment
 
+from django.urls import reverse
+from django.utils.html import format_html
+
 # Register your models here.
 
 
 class ReportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'reported_at', 'target']
+    list_display_links = ['status', 'target']
+    list_filter = ['status']
+
+    fields = ('status', 'reported_at', 'target_body')
+    readonly_fields = ('reported_at', 'target_body')
+
+    def target_body(self, obj):
+        if obj.post:
+            return obj.post.body
+        elif obj.comment:
+            return obj.comment.body
+    target_body.short_description = '신고 내용'
+    
+    def target(self, obj):
+        if obj.post:
+            link = reverse('admin:board_post_change', args=[obj.post.id])
+            return format_html('<a href="{}">{}</a>', link, f"게시글 신고 : {obj.post.body}")
+        elif obj.comment:
+            link = reverse('admin:board_comment_change', args=[obj.comment.id])
+            return format_html('<a href="{}">{}</a>', link, f"댓글 신고 : {obj.comment.body}")
+        
+    target.short_description = '신고글'
+    
+
     def save_model(self, request, obj, form, change):
         if change:
             if obj.post:
