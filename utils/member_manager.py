815af3279f4f819840136126fba88e9f491ddceb
leehjhjhj <tsukiakarii@naver.com>
2024-02-16 14:09:11 +0900

    refactor: member table 변경에 따른 manger 수정

--- utils/member_manager.py
+++ utils/member_manager.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import BaseUserManager
 
 class MemberManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
