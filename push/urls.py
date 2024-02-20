bb6d02db7b394f11dab642c0a6e129b93ab416da
leehjhjhj <tsukiakarii@naver.com>
2024-02-20 22:47:40 +0900

    add: push list url 추가

--- push/urls.py
+++ push/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from push.views import PushView, PushSendView, TokenInvalidView
+from push.views import (
+    PushView,
+    PushSendView, 
+    TokenInvalidView,
+    PushListGetVIew,
+)
 
 app_name = 'push'
 
 urlpatterns = [
     path('token/', PushView.as_view()),
     path('send/', PushSendView.as_view()),
-    path('token/invalid/', TokenInvalidView.as_view())
+    path('token/invalid/', TokenInvalidView.as_view()),
+    path('list/', PushListGetVIew.as_view()),
 ]
