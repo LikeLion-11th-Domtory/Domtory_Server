2bd003e9d37a6d8d0a8e8d1b434a67c9548d4efc
leehjhjhj <tsukiakarii@naver.com>
2024-02-20 19:20:37 +0900

    refactor: table name 인자 추가 및 키 이름 변경

--- utils/connect_dynamodb.py
+++ utils/connect_dynamodb.py
@@ -1,14 +1,14 @@
 from decouple import config
 import boto3
 
-def get_dynamodb_table():
-    AWS_ACCCESS_KEY=config('AWS_ACCCESS_KEY')
+def get_dynamodb_table(table_name):
+    AWS_ACCESS_KEY=config('AWS_ACCESS_KEY')
     AWS_SECRET_ACCESS_KEY=config('AWS_SECRET_ACCESS_KEY')
     
     dynamodb = boto3.resource(
         'dynamodb', region_name='ap-northeast-2',
-        aws_access_key_id=AWS_ACCCESS_KEY,
+        aws_access_key_id=AWS_ACCESS_KEY,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
-    return dynamodb.Table('clclcafe')
+    return dynamodb.Table(table_name)
     
