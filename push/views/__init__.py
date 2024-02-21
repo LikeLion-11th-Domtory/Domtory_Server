e92ff591db42df25c417ded070c1936e37092e53
leehjhjhj <tsukiakarii@naver.com>
2024-02-21 14:02:12 +0900

    feat: PushDeleteView 추가

--- push/views/__init__.py
+++ push/views/__init__.py
@@ -3,3 +3,4 @@ from .push_send_view import PushSendView
 from .token_invalid_view import TokenInvalidView
 from .push_list_get_view import PushListGetVIew
 from .push_check_view import PushCheckView
+from .push_delete_view import PushDeleteView
