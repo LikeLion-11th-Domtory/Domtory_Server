f086f728e482c01b27dff7e6ce37e7e2741ec3de
leehjhjhj <tsukiakarii@naver.com>
2024-03-05 00:59:04 +0900

    refactor: save에 data 저장하는 방식 변경 및 admin notification data 생성 로직 추가

---
+++ push/domains/admin_push_notification.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class AdminPushNotification(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    staff_member = models.ForeignKey('member.Member', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, help_text="제목을 넣어주세요. ex) 돔토리 업데이트 공지사항")
+    body = models.CharField(max_length=255, help_text="본문을 넣어주세요. ex) 1.10 버전이 새로 출시 되었으니 업데이트 바라요!")
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'admin_push_notification'
+        verbose_name = "관리자 푸시 알림"
+        verbose_name_plural = "관리자 푸시 알림"
