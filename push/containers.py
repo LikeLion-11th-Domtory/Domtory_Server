a4984073dfa4965f9a523cd7e186d28e58a10cf9
leehjhjhj <tsukiakarii@naver.com>
2024-03-19 00:55:33 +0900

    chore: notification_detail service에 대한 의존성 컨테이너 추가

--- push/containers.py
+++ push/containers.py
@@ -1,6 +1,6 @@
 from dependency_injector import containers, providers
-from push.services import PushService, DeviceService
-from push.domains import PushRepository
+from push.services import PushService, DeviceService, NotificationDetailService
+from push.domains import PushRepository, NotificationDetailRepository
 from board.repositories import BoardRepository
 from member.domains import MemberRepository
 
@@ -8,6 +8,8 @@ class PushContainer(containers.DeclarativeContainer):
     push_repository = providers.Factory(PushRepository)
     board_repository = providers.Factory(BoardRepository)
     member_repository = providers.Factory(MemberRepository)
+    notification_detail_repository = providers.Factory(NotificationDetailRepository)
+    
     push_service = providers.Factory(
         PushService,
         push_repository=push_repository,
@@ -18,4 +20,8 @@ class PushContainer(containers.DeclarativeContainer):
         DeviceService,
         push_repository=push_repository,
         board_repository=board_repository
+    )
+    notification_detail_service = providers.Factory(
+        NotificationDetailService,
+        notification_detail_repository=notification_detail_repository
     )
