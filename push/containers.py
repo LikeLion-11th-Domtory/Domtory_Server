ef8ce1bc0ecc7fe33d915484270869a5946c796e
leehjhjhj <tsukiakarii@naver.com>
2024-03-19 00:01:51 +0900

    add: push_sevice에 member_repository 추가

--- push/containers.py
+++ push/containers.py
@@ -2,14 +2,17 @@ from dependency_injector import containers, providers
 from push.services import PushService, DeviceService
 from push.domains import PushRepository
 from board.repositories import BoardRepository
+from member.domains import MemberRepository
 
 class PushContainer(containers.DeclarativeContainer):
     push_repository = providers.Factory(PushRepository)
     board_repository = providers.Factory(BoardRepository)
+    member_repository = providers.Factory(MemberRepository)
     push_service = providers.Factory(
         PushService,
         push_repository=push_repository,
-        board_repository=board_repository
+        board_repository=board_repository,
+        member_repository=member_repository
     )
     device_service = providers.Factory(
         DeviceService,
