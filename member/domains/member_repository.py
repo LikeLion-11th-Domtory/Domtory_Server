ef94b74a4afcc9256becf4941f503c480ebbdbf8
leehjhjhj <tsukiakarii@naver.com>
2024-03-18 23:52:20 +0900

    add: 멤버를 notification detail과 조인해서 가져오는 로직 추가

--- member/domains/member_repository.py
+++ member/domains/member_repository.py
@@ -8,5 +8,8 @@ class MemberRepository:
     def find_member_by_id(self, member_id: int):
         return get_object_or_404(Member, id=member_id)
     
+    def find_member_with_notification_detail_by_id(self, member_id: int):
+        return get_object_or_404(Member.objects.select_related('notificationdetail'), id=member_id)
+    
     def save_member(self, member: Member):
         member.save()
