e63f2af2e7f54460b4971b08cbe94c401c4944e4
leehjhjhj <tsukiakarii@naver.com>
2024-03-25 10:00:00 +0900

    feat: notification detail 저장 로직 추가

--- push/domains/notification_detail_repository.py
+++ push/domains/notification_detail_repository.py
@@ -5,3 +5,6 @@ class NotificationDetailRepository:
 
     def find_notification_detail_by_member_id(self, member_id: int):
         return get_object_or_404(NotificationDetail, member_id=member_id)
+    
+    def save_notification_detail(self, notification_detail: NotificationDetail):
+        notification_detail.save()
