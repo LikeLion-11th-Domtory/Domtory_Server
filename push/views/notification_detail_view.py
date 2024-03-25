e1213f64761427293ab553964d5f4fa6b25fc761
leehjhjhj <tsukiakarii@naver.com>
2024-03-25 10:00:00 +0900

    feat: notification detail get 뷰 추가

--- push/views/notification_detail_view.py
+++ push/views/notification_detail_view.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from push.containers import PushContainer
 from drf_yasg.utils import swagger_auto_schema
-from push.serializers import NotificationDetailResponseSerializer
+from push.serializers import NotificationDetailSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from drf_yasg import openapi
@@ -24,10 +24,18 @@ class NotificationDetailGetView(APIView):
         super().__init__(*args, **kwargs)
         self._notification_detail_service = PushContainer.notification_detail_service()
 
-    @swagger_auto_schema(manual_parameters=[authorization_header], responses={"200": NotificationDetailResponseSerializer})
+    @swagger_auto_schema(manual_parameters=[authorization_header], responses={"200": NotificationDetailSerializer})
     def get(self, request):
         """
-        푸시 알람 기록 리스트를 가져오는 api 입니다. 총 20개 불러옵니다.
+        푸시 상세를 조회하는 api
         """
         response = self._notification_detail_service.get_notification_detail(request.user)
         return Response(response, status=status.HTTP_200_OK)
+    
+    @swagger_auto_schema(manual_parameters=[authorization_header],request_body=NotificationDetailSerializer, responses={"200": ""})
+    def put(self, request):
+        """
+        푸시 상세를 수정하는 api
+        """
+        self._notification_detail_service.change_notification_detail(request.data, request.user)
+        return Response(status=status.HTTP_200_OK)
