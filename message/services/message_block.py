abc195272011bb3af6d34abd995f0a1ff4da3df2
Zena0128 <0128ja@gmail.com>
2024-05-01 01:40:46 +0900

    feat: 중복차단 방지 로직 추가

--- message/services/message_block.py
+++ message/services/message_block.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from member.domains import Member
 from message.domains.message_block_models import MessageBlock
 from message.serializers.message_serializer import MessageBlockRequestSerializer, MessageBlockResponseSerializer
+from utils.exceptions.message_exception import AlreadyBlockedError
 
 
 def get_message_block(request, message_block_id):
@@ -13,7 +14,18 @@ def get_message_block(request, message_block_id):
 
 def create_message_block(request, target_id):
     target = Member.objects.get(id = target_id)
-    serializer = MessageBlockRequestSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    block = serializer.save(req_id=request.user, tar_id=target)
-    return get_message_block(request, block.id)
+    try:
+        block = MessageBlock.objects.get(req_id=request.user, tar_id=target)
+    except MessageBlock.DoesNotExist:
+        block = None
+    try:
+        blocked = MessageBlock.objects.get(req_id=target, tar_id=request.user)
+    except MessageBlock.DoesNotExist:
+        blocked = None
+    if not block and not blocked:
+        serializer = MessageBlockRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        block = serializer.save(req_id=request.user, tar_id=target)
+        return get_message_block(request, block.id)
+    else:
+        raise AlreadyBlockedError
--- utils/exceptions/message_exception.py
+++ utils/exceptions/message_exception.py
@@ -8,3 +8,7 @@ class MessageBlockedError(APIException):
 class MessageToMeError(APIException):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = "자기 자신에게 쪽지를 보낼 수 없습니다."
+
+class AlreadyBlockedError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "이미 차단된 이용자입니다."
