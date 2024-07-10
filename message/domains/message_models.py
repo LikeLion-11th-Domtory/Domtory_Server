a6c5fd42e7225efda30f2e9f7ffd968631506a17
leehjhjhj <tsukiakarii@naver.com>
2024-07-10 23:05:47 +0900

    refactor: str 메서드 삭제

--- message/domains/message_models.py
+++ message/domains/message_models.py
@@ -32,8 +32,5 @@ class Message(models.Model):
     is_deleted_send = models.BooleanField(default=False)
     is_deleted_recv = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.body
-
     class Meta:
         db_table = 'message'
