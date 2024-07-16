a355e13c005aad7574c1ec16e12f4363033e4988
Chaeyoung714 <202102760@hufs.ac.kr>
2024-07-16 13:51:35 +0900

    feat: report.admin에 유저 차단 기능 추가

--- report/admin.py
+++ report/admin.py
@@ -9,50 +9,30 @@ from django.utils.html import format_html
 
 # Register your models here.
 
-# class PostInline(admin.TabularInline):
-#     model = Post
-#     fields = ('status',)
-
-# class CommentInline(admin.TabularInline):
-#     model = Comment
-#     fields = ('status',)
-
 
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ['id', 'status', 'reported_at', 'target', 'user_status',]
-    list_display_links = ['status', 'target', 'user_status',]
+    list_display = ['id', 'status', 'reported_at', 'target',]
+    list_display_links = ['status', 'target',]
     list_filter = ['status']
 
-    # actions = ["action_change_valid", "action_change_invalid", "action_change_banned"]
+    actions = ["action_change_valid", "action_change_invalid", ##Report.status의 상태변화
+               "action_change_banned", "action_change_active"] ##Report.ismemberblocked의 상태변화 
 
-    fields = ('status', 'reported_at', 'target_body',)
+    fields = ('status', 'reported_at', 'target_body', 'member_status',)
     readonly_fields = ('reported_at', 'target_body',)
 
     # inlines = [PostInline, CommentInline]
 
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        # 필요한 경우 추가적인 데이터 처리
-        return queryset.select_related('parent')  
-
-    # 가져올 쿼리셋 정의
-    def get_form_queryset(self, obj):
-        return self.model.objects.filter(email=obj.email)
-	
-    # Inline에서 정의한 모델이 저장되는 경우에 동작
-    def save_new_instance(self, parent, instance):
-        instance.email = parent.email
-
 
     ## 신고 detail
-    def status_user(self, obj):
-        if obj.post:
-            member = Member.objects.get(pk=obj.post.member.id)
-            return member.status #쿼리가..
-        elif obj.comment:
-            member = Member.objects.get(pk=obj.comment.member.id)
-            return member.status
-    status_user.short_description = '유저 정지'
+    # def status_member(self, obj):
+    #     if obj.post:
+    #         member = Member.objects.get(pk=obj.post.member.id)
+    #         return member.status #쿼리가..
+    #     elif obj.comment:
+    #         member = Member.objects.get(pk=obj.comment.member.id)
+    #         return member.status
+    # status_user.short_description = '유저 정지'
 
     def target_body(self, obj):
         if obj.post:
@@ -75,13 +55,13 @@ class ReportAdmin(admin.ModelAdmin):
         
     target.short_description = '신고글'
 
-    def user_status(self, obj):
-        if obj.post:
-            link = reverse('admin:member_member_change', args=[obj.post.id])
-            return format_html('<a href="{}">{}</a>', link, f"유저")
-        elif obj.comment:
-            link = reverse('admin:member_member_change', args=[obj.comment.id])
-            return format_html('<a href="{}">{}</a>', link, f"유저")
+    # def user_status(self, obj):
+    #     if obj.post:
+    #         link = reverse('admin:member_member_change', args=[obj.post.id])
+    #         return format_html('<a href="{}">{}</a>', link, f"유저")
+    #     elif obj.comment:
+    #         link = reverse('admin:member_member_change', args=[obj.comment.id])
+    #         return format_html('<a href="{}">{}</a>', link, f"유저")
     
     
 
@@ -93,43 +73,70 @@ class ReportAdmin(admin.ModelAdmin):
 
     action_change_valid.short_description = "선택한 신고의 상태를 유효한 신고로 바꿉니다."
     
-    def action_change_invalid(self, request, queryset):
-        queryset.update(status=Report.ReportType.INVALID)
+    # def action_change_invalid(self, request, queryset):
+    #     queryset.update(status=Report.ReportType.INVALID)
     
-    action_change_invalid.short_description = "선택한 신고의 상태를 유효하지 않은 신고로 바꿉니다."
+    # action_change_invalid.short_description = "선택한 신고의 상태를 유효하지 않은 신고로 바꿉니다."
 
 
     ### 유저 밴
-    # def action_change_banned(self, request, queryset):
-    #     for report in queryset:
-    #         if report.post:
-    #             report.post.member.status = Member.MEMBER_STATUS_CHOICES['BANNED']
-    #         elif report.comment:
-    #             report.comment.member.status = Member.MEMBER_STATUS_CHOICES['BANNED']
-    #         self.save_model(request, report, None, True)
+    def action_change_banned(self, request, queryset):
+        for report in queryset:
+            if report.post:
+                report.member_status = Report.MemberType.BANNED
+            elif report.comment:
+                report.member_status = Report.MemberType.BANNED
+            self.save_model(request, report, None, True)
 
-    # action_change_valid.short_description = "선택한 유저를 차단합니다."
+    action_change_banned.short_description = "작성자 유저를 차단합니다."
     
     # def action_change_active(self, request, queryset):
-    #     queryset.update(status_user=Member.MEMBER_STATUS_CHOICES['BANNED'])
+    #     for report in queryset:
+    #         if report.post:
+    #             report.is_member_blocked = False
+    #             report.post.member.status = Member.MEMBER_STATUS_CHOICES['ACTIVE']
+    #         elif report.comment:
+    #             report.is_member_blocked = False
+    #             report.comment.member.status = Member.MEMBER_STATUS_CHOICES['ACTIVE']
+    #         self.save_model(request, report, None, True)
     
-    # action_change_invalid.short_description = "선택한 유저의 차단을 해제합니다."
-
-
+    # action_change_active.short_description = "작성자 유저의 차단을 해제합니다."
 
 
     def save_model(self, request, obj, form, change):
         if change:
             if obj.post:
-                if obj.status == Report.ReportType.VALID:
+                if obj.status == Report.ReportType.VALID: # 유효한 신고일 때 - 신고글 정지
                     obj.post.is_blocked = True
+
+                if obj.member_status == Report.MemberType.BANNED: # 유저 정지 했을때 - 유저, 신고글 함께 정지
+                    obj.post.member.status = Member.MEMBER_STATUS_CHOICES[1]
+                    obj.status = Report.ReportType.VALID
+                    obj.post.is_blocked = True
+                
                 obj.post.save()
+                obj.post.member.save()
+            
             if obj.comment:
-                if obj.status == Report.ReportType.VALID:
+                if obj.status == Report.ReportType.VALID: # 유효한 신고일 때 - 신고글 정지
+                    obj.comment.is_blocked = True
+
+                if obj.member_status == Report.MemberType.BANNED: # 유저 정지 했을때 - 유저, 신고글 함께 정지
+                    obj.comment.member.status = Member.MEMBER_STATUS_CHOICES[1]
+                    obj.status = Report.ReportType.VALID
                     obj.comment.is_blocked = True
                 obj.comment.save()
+                obj.comment.member.save()
 
         super().save_model(request, obj, form, change)
+
+    """"
+    MEMBER_STATUS_CHOICES = (
+        ('ACTIVE','활동'),
+        ('BANNED', '정지'),
+        ('WITHDRAWAL', '탈퇴')
+    )
+    """
 
 
 admin.site.register(Report, ReportAdmin)
--- report/models/report_models.py
+++ report/models/report_models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from board.models import Comment, Post
 from message.domains.message_models import Message
+from member.models import Member
 
 
 # Create your models here.
@@ -11,12 +12,16 @@ class Report(models.Model):
         PENDING = "PENDING", "관리자 확인 대기"
         VALID = "VALID", "유효한 신고"
         INVALID = "INVALID", "유효하지 않은 신고"
+    class MemberType(models.TextChoices):
+        BANNED = 'BANNED', "유저 정지"
+        ACTIVE = 'ACTIVE', "유저 정지 해제"
     status = models.CharField(choices=ReportType.choices, default="WAITING", max_length=10)
     reported_at = models.DateTimeField(auto_now_add=True)
     # report_type = 
     post = models.ForeignKey(Post, null=True, blank=True, on_delete=models.SET_NULL)
     comment = models.ForeignKey(Comment, null=True, blank=True, on_delete=models.SET_NULL)
     message = models.ForeignKey(Message, null=True, blank=True, on_delete=models.SET_NULL)
+    member_status = models.CharField(choices=MemberType.choices, default="ACTIVE", max_length=10)
 
     def __str__(self):
         if self.post:
