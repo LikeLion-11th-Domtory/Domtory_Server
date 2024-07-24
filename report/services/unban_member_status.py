3b6f9539dcba462fd9886cea383b1131c96e87d6
Chaeyoung714 <202102760@hufs.ac.kr>
2024-07-24 15:07:36 +0900

    comment: 주석추가

--- report/services/unban_member_status.py
+++ report/services/unban_member_status.py
@@ -8,6 +8,6 @@ def async_unban_member_status(report_id, ban_period):
     print("member_id = ", report_id, "& ban_period = ", ban_period)
 
     # countdown = int(ban_period) * 60 * 60 * 24 #ban_period(3, 7, 30일)을 초로 변환
-    countdown = int(ban_period) * 60
+    countdown = int(ban_period) * 60 #테스트용
     unban_member_task.apply_async((report_id,), countdown=countdown)
     return
