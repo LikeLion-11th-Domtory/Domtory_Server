596d4284a99e500ba55d29bc4d53f58735ee007d
ShinSub98 <wjdtjq26@naver.com>
2024-07-30 04:36:50 +0900

    feat: 기숙사별 게시글 조회 추가

--- board/urls.py
+++ board/urls.py
@@ -11,10 +11,14 @@ urlpatterns = [
     path('post/detail/<int:post_id>/', PostDetailView.as_view()), # 게시글 상세정보
     path('post/update/<int:post_id>/', PostUpdateView.as_view()), # 게시글 수정
     path('post/delete/<int:post_id>/', PostDeleteView.as_view()), # 게시글 삭제
-    path('post/list/<int:board_id>/', PostListView.as_view()), # 특정 게시판의 게시글 리스트
-    path('post/latest/<int:board_id>/', FreeBoardSimpleView.as_view()), # 특정 게시판의 최근 5개 게시글
+    path('post/list/<int:board_id>/', PostListByDormView.as_view()), # 기숙사별 특정 게시판의 게시글 리스트
+    path('post/latest/<int:board_id>/', RecentPostListView.as_view()), # 기숙사별 특정 게시판의 최근 5개 게시글
     path('post/search/<int:board_id>/', PostSearchView.as_view()), # 게시글 검색
     path('post/create/council/', CouncilPostCreateView.as_view()), # 자율회 게시글 생성
+
+    ## deprecated
+    # path('post/list/<int:board_id>/', PostListView.as_view()), # 특정 게시판의 게시글 리스트
+    # path('post/latest/<int:board_id>/', FreeBoardSimpleView.as_view()), # 특정 게시판의 최근 5개 게시글
 
     # 댓글 API
     path('comment/create/<int:post_id>/', CommentCreateView.as_view()), # 댓글 작성
