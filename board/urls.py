e69d4b447c808775579b5c95329c6635c72ceb7a
ShinSub98 <wjdtjq26@naver.com>
2024-07-30 05:22:20 +0900

    feat: 기숙사에 맞는 게시판을 조회할 수 있도록 게시글 CRUD 추가

--- board/urls.py
+++ board/urls.py
@@ -7,18 +7,20 @@ app_name = 'board'
 
 urlpatterns = [
     # 게시글 API
-    path('post/create/<int:board_id>/', PostCreateView.as_view()), # 게시글 생성
+    path('post/create/<int:board_id>/', PostCreateViewV2.as_view()), # 게시글 생성
     path('post/detail/<int:post_id>/', PostDetailView.as_view()), # 게시글 상세정보
     path('post/update/<int:post_id>/', PostUpdateView.as_view()), # 게시글 수정
     path('post/delete/<int:post_id>/', PostDeleteView.as_view()), # 게시글 삭제
     path('post/list/<int:board_id>/', PostListByDormView.as_view()), # 기숙사별 특정 게시판의 게시글 리스트
     path('post/latest/<int:board_id>/', RecentPostListView.as_view()), # 기숙사별 특정 게시판의 최근 5개 게시글
-    path('post/search/<int:board_id>/', PostSearchView.as_view()), # 게시글 검색
+    path('post/search/<int:board_id>/', PostSearchViewV2.as_view()), # 게시글 검색
     path('post/create/council/', CouncilPostCreateView.as_view()), # 자율회 게시글 생성
 
     ## deprecated
     # path('post/list/<int:board_id>/', PostListView.as_view()), # 특정 게시판의 게시글 리스트
     # path('post/latest/<int:board_id>/', FreeBoardSimpleView.as_view()), # 특정 게시판의 최근 5개 게시글
+    # path('post/create/<int:board_id>/', PostCreateView.as_view()), # 게시글 생성
+    # path('post/search/<int:board_id>/', PostSearchView.as_view()), # 게시글 검색
 
     # 댓글 API
     path('comment/create/<int:post_id>/', CommentCreateView.as_view()), # 댓글 작성
@@ -37,9 +39,13 @@ urlpatterns = [
     path('post/paged/search/<int:board_id>/', PaginatedPostSearchView.as_view()), # 검색 결과를 페이지네이션하여 반환
 
     # 좋아요, 핫게 관련 API
-    path('post/paged/list/popular/', PopularBoardListView.as_view()), #핫게시판 게시글 리스트 (페이지네이션)
+    path('post/paged/list/popular/', PopularBoardListViewV2.as_view()), #핫게시판 게시글 리스트 (페이지네이션)
     path('post/like/<int:post_id>/', PostLikeView.as_view()), #게시물 좋아요
     path('comment/like/<int:comment_id>/', CommentLikeView.as_view()), #댓글 좋아요
+
+
+    ## deprecated
+    # path('post/paged/list/popular/', PopularBoardListView.as_view()), #핫게시판 게시글 리스트 (페이지네이션)
 
     # 스크랩 API
     path('post/bookmark/<int:post_id>/', BookmarkPostView.as_view()), # 게시글을 스크랩
