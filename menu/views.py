c27fdb397881807b246acae5ab323f128f1eb542
ShinSub98 <wjdtjq26@naver.com>
2024-09-01 15:32:22 +0900

    <feat>: 사용자의 소속 기숙사에 따른 메뉴 필터링 추가

--- menu/views.py
+++ menu/views.py
@@ -3,6 +3,8 @@ import datetime
 from .models import Menu
 from .serializers import MenuListSerializer, BreakfastListSerializer, LunchListSerializer, DinnerListSerializer
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import *
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,14 +19,16 @@ def find_sunday(date_code):
 
 # 날짜에 맞는 식단 조회: total이면 전부 조회, 아니면 아침, 점심, 저녁 중 하나만 조회
 class MenuListView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self, request, date_code, option):
         if option == 'total':
             sunday = find_sunday(date_code)
             saturday = (datetime.datetime.strptime(sunday, '%y%m%d').date() + datetime.timedelta(days=6)).strftime('%y%m%d')
-            menus = Menu.objects.filter(date_code__gte=sunday, date_code__lte=saturday)
+            menus = Menu.objects.filter(date_code__gte=sunday, date_code__lte=saturday, dorm = request.user.dorm)
             serializer = MenuListSerializer(menus, many=True)
         else:
-            menus = Menu.objects.filter(date_code=date_code)
+            menus = Menu.objects.filter(date_code=date_code, dorm = request.user.dorm)
             if option == 'breakfast':
                 serializer = BreakfastListSerializer(menus, many=True)
             elif option == 'lunch':
