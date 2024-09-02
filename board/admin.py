f2c746cbb23a376a59e2b8abb1fba16bdd623266
ShinSub98 <wjdtjq26@naver.com>
2024-09-02 21:55:21 +0900

    <refactor>: 전체 기숙사 관리자가 모든 board 데이터를 조회할 수 있도록 수정

--- board/admin.py
+++ board/admin.py
@@ -32,7 +32,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'member__name']
 
     def get_queryset(self, request):
-        if request.user.is_superuser:
+        if request.user.is_superuser or request.user.dorm_id == Dorm.DORM_LIST[0][1]:
             return super().get_queryset(request)
         queryset = super().get_queryset(request)
         return queryset.filter(Q(dorm_id = request.user.dorm.id)|Q(board_id = 7))
@@ -51,7 +51,7 @@ class PostImageAdmin(admin.ModelAdmin):
     search_fields = ['post__title']
 
     def get_queryset(self, request):
-        if request.user.is_superuser:
+        if request.user.is_superuser or request.user.dorm_id == Dorm.DORM_LIST[0][1]:
             return super().get_queryset(request)
         queryset = PostImage.objects.select_related('post').filter(Q(dorm_id = request.user.dorm.id)|Q(post__board_id = 7))
         return queryset
@@ -68,7 +68,7 @@ class CommentAdmin(admin.ModelAdmin):
     fields = ('post', 'parent', 'body')
 
     def get_queryset(self, request):
-        if request.user.is_superuser:
+        if request.user.is_superuseror or request.user.dorm_id == Dorm.DORM_LIST[0][1]:
             return super().get_queryset(request)
         queryset = Comment.objects.select_related('post').filter(Q(dorm_id = request.user.dorm.id)|Q(post__board_id = 7))
         return queryset
